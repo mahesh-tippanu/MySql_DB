44473c5ee817e15fc59620905f872b879b20d193
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-05-31 18:00:25 +0530

    added UC2_CreateTableEmployeePayroll.sql

--- Mysql_Payroll_service.sql
+++ Mysql_Payroll_service.sql
@@ -1,2 +1,10 @@
 create schema payroll_service;
 use payroll_service;
+CREATE TABLE employee_payroll (
+	id int NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    salary double NOT NULL,
+    start_date DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+DESC employee_payroll
